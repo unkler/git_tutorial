eab73fe14560fc0494ac1a7abd5a602c3eebddc8
unkler <info@unkler.net>
2019-11-25 19:13:27 +0900

    CSVFileReader.phpにread関数を追加

--- CSVFileReader.php
+++ CSVFileReader.php
@@ -4,32 +4,38 @@ require_once("Reader.class.php");
 /**
  * CSV読み込みクラス
  */
-class CSVFileReader implements Reader
-{
-    /**
-     * 内容を表示するファイル名
-     *
-     * @access private
-     */
-    private $filename;
+class CSVFileReader implements Reader　{
+  /**
+   * 内容を表示するファイル名
+   *
+   * @access private
+   */
+  private $filename;
 
-    /**
-     * データを扱うハンドラ名
-     *
-     * @access private
-     */
-    private $handler;
+  /**
+   * データを扱うハンドラ名
+   *
+   * @access private
+   */
+  private $handler;
 
-    /**
-     * コンストラクタ
-     *
-     * @param string ファイル名
-     * @throws Exception
-     */
-    public function __construct($filename) {
-        if (!is_readable($filename)) {
-            throw new Exception('file "' . $filename . '" is not readable !');
-        }
-        $this->filename = $filename;
-    }
+  /**
+   * コンストラクタ
+   *
+   * @param string ファイル名
+   * @throws Exception
+   */
+  public function __construct($filename) {
+      if (!is_readable($filename)) {
+          throw new Exception('file "' . $filename . '" is not readable !');
+      }
+      $this->filename = $filename;
   }
+
+  /**
+ * 読み込み関数
+ */
+　public function read()　{
+      $this->handler = fopen ($this->filename, "r");
+  }
+}
