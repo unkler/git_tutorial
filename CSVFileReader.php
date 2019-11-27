3ac35a201bcaa81fbc208ea24885541f43528d8a
unkler <info@unkler.net>
2019-11-27 22:00:13 +0900

    display関数を追加

--- CSVFileReader.php
+++ CSVFileReader.php
@@ -38,4 +38,34 @@ class CSVFileReader implements Reader　{
 　public function read()　{
       $this->handler = fopen ($this->filename, "r");
   }
+
+  /**
+ * 表示関数
+ */
+  public function display() {
+     $column = 0;
+     $tmp = "";
+    while ($data = fgetcsv ($this->handler, 1000, ",")) {
+         $num = count ($data);
+         for ($c = 0; $c < $num; $c++) {
+             if ($c == 0) {
+                 if ($column != 0 && $data[$c] != $tmp) {
+                     echo "</ul>";
+                 }
+                 if ($data[$c] != $tmp) {
+                     echo "<b>" . $data[$c] . "</b>";
+                     echo "<ul>";
+                     $tmp = $data[$c];
+                 }
+             }else {
+                 echo "<li>";
+                 echo $data[$c];
+                 echo "</li>";
+             }
+         }
+         $column++;
+     }
+     echo "</ul>";
+     fclose ($this->handler);
+ }
 }
