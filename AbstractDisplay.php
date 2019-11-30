e9bf70c5168fa78c008da9dfa8c794e60411d2a2
unkler <info@unkler.net>
2019-11-30 20:43:17 +0900

    AbstractDisplay.phpを追加

---
+++ AbstractDisplay.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * AbstractDisplayクラス
+ */
+abstract class AbstractDisplay {
+    /**
+     * 表示するデータ
+     */
+    private $data;
+
+    /**
+     * コンストラクタ
+     * @param mixed 表示するデータ
+     */
+    public function __construct($data)　{
+        if (!is_array($data)) {
+            $data = array($data);
+        }
+        $this->data = $data;
+    }
+
+    /**
+     * template methodに相当
+     */
+    public function display()　{
+        $this->displayHeader();
+        $this->displayBody();
+        $this->displayFooter();
+    }
+
+    /**
+     * データを取得
+     */
+    public function getData()　{
+        return $this->data;
+    }
+
+    /**
+     * ヘッダ表示
+     * サブクラスに実装を任せる抽象メソッド
+     */
+    protected abstract function displayHeader();
+
+    /**
+     * ボディ（クライアントから渡された内容）表示
+     * サブクラスに実装を任せる抽象メソッド
+     */
+    protected abstract function displayBody();
+
+    /**
+     * フッタを表示
+     * サブクラスに実装を任せる抽象メソッド
+     */
+    protected abstract function displayFooter();
+
+}
